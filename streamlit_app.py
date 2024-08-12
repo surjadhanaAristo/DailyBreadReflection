5bc6eb8b5cf087bd5f9eb4f2fe37b21238f4fa4b
Aristo <33140521+surjadhanaAristo@users.noreply.github.com>
2024-08-12 14:19:03 -0400

    Update import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
-import openai
+from openai import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
