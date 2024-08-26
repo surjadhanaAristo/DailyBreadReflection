2204910219de579033368350a7dc85300f9f4445
Aristo <33140521+surjadhanaAristo@users.noreply.github.com>
2024-08-26 13:02:31 -0400

    changed csv again

--- sitetest.py
+++ sitetest.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 today = datetime.today()
-url = "https://github.com/surjadhanaAristo/DailyBreadReflection/blob/main/newcsv.numbers?raw=true"
-df = pd.read_csv(url, index_col=1)
+url = "https://github.com/surjadhanaAristo/DailyBreadReflection/blob/main/newcsv.csv"
+df = pd.read_csv(url, index_col=0)
 apidate = today.strftime("%Y-%m-%d")
 new_date = df[0]
