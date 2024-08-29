c5ec617ab146f6c5891c7ef96e568316f7ce8930
Aristo <darkvale57@gmail.com>
2024-08-29 13:36:50 -0400

    removed sitetest

--- sitetest.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-from datetime import datetime
-today = datetime.today()
-url = "https://github.com/surjadhanaAristo/DailyBreadReflection/blob/main/new.csv?raw=true"
-df = pd.read_csv(url, index_col=0)
-apidate = today.strftime("%Y-%m-%d")
-new_date = df[apidate][0]
-print(new_date)
